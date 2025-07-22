fc6c6099a67b97e72df14e2f3cbaa0dcb787cd9e
Travis <tlneondo@gmail.com>
2025-07-22 13:38:26 -0400

    initial commit

---
+++ SteamOverlaySync.sh
@@ -0,0 +1,47 @@
+sudo mkdir /mnt/winOverlay
+cd /mnt/winOverlay
+sudo mkdir SSDWinlower SSDWinupper SSD2Winlower SSD2Winupper SSDWinMerge SSD2WinMerge
+
+#mounts for lower readonly ntfs
+UUID=82C425D7C425CDEB /mnt/winOverlay/SSDWinlower ntfs ro,windows_names,prealloc 0 0
+UUID=... /mnt/winOverlay/SSD2Winlower ntfs ro,windows_names,prealloc 0 0
+
+##set up btrfs subvolume for upper layer
+sudo btrfs subvolume create /mnt/winOverlay/@SSDWinupper
+sudo btrfs subvolume create /mnt/winOverlay/@SSD2Winupper
+
+UUID=... /mnt/winOverlay/SSDWinupper btrfs defaults 0 0
+UUID=... /mnt/winOverlay/SSD2Winupper btrfs defaults 0 0
+
+overlay /mnt/SSDWin overlay noauto,x-systemd.automount,lowerdir=/mnt/winOverlay/SSDWinlower,upperdir=/mnt/winOverlay/SSDWinupper,workdir=/mnt/winOverlay/SSDWinMerge
+overlay /mnt/SSD2Win overlay noauto,x-systemd.automount,lowerdir=/mnt/winOverlay/SSD2Winlower,upperdir=/mnt/winOverlay/SSD2Winupper,workdir=/mnt/winOverlay/SSD2WinMerge
+
+
+#merge script
+
+#
+# TODO Wait to make sure filesystems are not busy
+#
+
+#close steam
+killall steam
+
+#unmount merged folders
+sudo umount /mnt/SSDWin
+sudo umount /mnt/SSD2Win
+
+#unmount lower readonly ntfs
+sudo umount /mnt/winOverlay/SSDWinlower
+sudo umount /mnt/winOverlay/SSD2Winlower
+
+#mount NTFS as read write
+sudo mount rw /mnt/winOverlay/SSDWinlower
+sudo mount rw /mnt/winOverlay/SSD2Winlower
+
+#use overlayfs tools to merge changes
+sudo rsync -a --delete /mnt/winOverlay/SSDWinupper/ /mnt/winOverlay/SSDWinlower/
+
+or overlay merge /mnt/winOverlay/SSDWinupper/ /mnt/winOverlay/SSDWinlower/
+
+
+
