f13fb44bbf7f8fa724c2263800c9e391c7b7d2d9
Travis <tlneondo@gmail.com>
2025-07-23 09:44:32 -0400

    replace sleeps with user prompts

--- SteamOverlaySync.sh
+++ SteamOverlaySync.sh
@@ -9,37 +9,41 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+cd /
+
 
 echo "Closing Steam"
 killall steam
 
-sleep 10s
+read -n 1 -s -r -p "Press any key when Steam is closed"
 
 echo"unmount merged folders"
 sudo umount /mnt/SSDWin
 sudo umount /mnt/SSD2Win
 
-sleep 10s
+read -n 1 -s -r -p "Press any key when folders are unmounted"
 
 echo "mount NTFS as read write"
 
 sudo mount UUID=82C425D7C425CDEB /mnt/winOverlay/SSDWinLower -o remount,rw,windows_names,prealloc
 sudo mount UUID=78DBFD1A57D3E447 /mnt/winOverlay/SSD2WinLower -o remount,rw,windows_names,prealloc
 
-sleep 15s
+read -n 1 -s -r -p "Press any key if drives mounted correctly"
 
 echo "use overlayfs tools to merge changes"
 sudo ./overlay merge -l /mnt/winOverlay/SSDWinLower/SteamLibrary/steamapps/ -u /mnt/winOverlay/SSDWinUpper/SteamLibrary/steamapps/ -f
 sudo ./overlay merge -l /mnt/winOverlay/SSD2WinLower/SteamLibrary/steamapps/ -u /mnt/winOverlay/SSD2WinUpper/SteamLibrary/steamapps/ -f
 
+read -n 1 -s -r -p "Press any key to no errors have occurred in overlay merge"
+
+cd /
+
 echo "unmount writable ntfs"
 sudo umount /mnt/winOverlay/SSDWinLower
 sudo umount /mnt/winOverlay/SSD2WinLower
 
-sleep 15s
+read -n 1 -s -r -p "Press any key when writable lower drives are unmounted"
 
-
-sleep 15s
 
 echo "remount drives from fstab"
 sudo mount -a
