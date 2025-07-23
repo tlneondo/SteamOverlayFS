24c11d723b3088cbf2311b2c9053b784ce361b85
tlneondo <tlneondo@gmail.com>
2025-07-23 19:06:48 -0400

    remove spaces around variable

--- SteamOverlaySync.sh
+++ SteamOverlaySync.sh
@@ -63,8 +63,8 @@ else
 fi
 
 #check if disk space in layer is less than free space on drive
-amtinLayer = $(df --total | grep "/mnt/winOverlay/SSDWinUpper" | awk '{printf "%s\n",$3}')
-amtFreeOnDrive = $(df --total | grep "/mnt/winOverlay/SSDWinLower" | awk '{printf "%s\n",$4}')
+amtinLayer=$(df --total | grep "/mnt/winOverlay/SSDWinUpper" | awk '{printf "%s\n",$3}')
+amtFreeOnDrive=$(df --total | grep "/mnt/winOverlay/SSDWinLower" | awk '{printf "%s\n",$4}')
 
 if(amtinLayer -gt amtFreeOnDrive); then
     echo "Not enough space on drive to merge changes, exiting"
