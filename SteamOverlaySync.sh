137dcab250ac99011bc152dcc4c98fa4feed0667
Travis <tlneondo@gmail.com>
2025-07-24 10:44:28 -0400

    1.0? of systemd version?

--- SteamOverlaySync.sh
+++ SteamOverlaySync.sh
@@ -2,151 +2,80 @@
 
 #merge script
 
-#
-# TODO Wait to make sure filesystems are not busy
-#
+echo "Script Start: Merge OverlayFS into NTFS Drive" | systemd-cat -t sysDSyncSteamb4Shutdown
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root or with sudo. Exiting."
+   echo "This script must be run as root or with sudo. Exiting." | systemd-cat -t sysDSyncSteamb4Shutdown
    exit 1
 fi
 
 # Get the directory of the current script
-    SCRIPT_DIR="$(dirname "$0")"
-
-
-
-killprocesses() {
-    # Check if any processes are using the mount points
-    local mount_points=("/mnt/SSDWin" "/mnt/SSD2Win" "/mnt/winOverlay/SSDWinLower" "/mnt/winOverlay/SSD2WinLower")
-    for mount_point in "${mount_points[@]}"; do
-        fuser -vcuk "$mount_point"
-    done
-
-    # Wait a moment to ensure processes have terminated
-    sleep 2
-    #check again
-    for mount_point in "${mount_points[@]}"; do
-        if lsof +D "$mount_point" &>/dev/null; then
-            echo "Processes are using the mount point $mount_point. Please close them before proceeding."
-            exit 1
-        fi
-    done
-}
+SCRIPT_DIR="$(dirname "$0")"
 
 
 #check if disk space in layer is less than free space on drive
 amtinLayer=$(du -c -d 0 /mnt/winOverlay/SSDWinUpper | grep "total" | awk '{printf "%s",$1}')
 amtFreeOnDrive=$(df --total | grep "/mnt/winOverlay/SSDWinLower" | awk '{printf "%s",$4}')
 
-printf "Amount in layer: %s\n" "$amtinLayer"
-printf "Amount free on drive: %s\n" "$amtFreeOnDrive" 
+printf "Amount in layer: %s\n" "$amtinLayer" | systemd-cat -t sysDSyncSteamb4Shutdown
+printf "Amount free on drive: %s\n" "$amtFreeOnDrive" | systemd-cat -t sysDSyncSteamb4Shutdown
 
 
 if [[ $amtinLayer -gt $amtFreeOnDrive ]]
    then
-      echo "Not enough space on drive to merge changes, exiting"
+      echo "Not enough space on drive to merge changes, exiting" | systemd-cat -t sysDSyncSteamb4Shutdown
       exit 1
 fi
 
-echo "Closing Steam"
-killall steam
 
-#kill everything using the mount points
-#echo "Killing processes using mount points"
-#killprocesses
-
+echo "unmounting layers"  | systemd-cat -t sysDSyncSteamb4Shutdown
 sudo umount /mnt/SSDWin
 sudo umount /mnt/SSD2Win
 
-sleep 2
 
-echo "unmount lower read only ntfs drives"
+echo "unmount lower read only ntfs drives"  | systemd-cat -t sysDSyncSteamb4Shutdown
 sudo umount /mnt/winOverlay/SSDWinLower
-sudo umount /mnt/winOverlay/SSD2WinLower
-
-sleep 2
+sudo umount /mnt/winOverlay/SSDWinLower
 
 #check that the the unmounting was successful
 if mountpoint /mnt/winOverlay/SSDWinLower; then
-    echo "SSDWinLower is still mounted, exiting"
+    echo "SSDWinLower is still mounted, exiting" | systemd-cat -t sysDSyncSteamb4Shutdown
     exit 1
 fi
 
 if mountpoint /mnt/winOverlay/SSD2WinLower; then
-    echo "SSD2WinLower is still mounted, exiting"
+    echo "SSD2WinLower is still mounted, exiting" | systemd-cat -t sysDSyncSteamb4Shutdown
     exit 1
 fi
 
-
+#mount the upper layers read write
 sudo mount UUID=82C425D7C425CDEB /mnt/winOverlay/SSDWinLower -o rw,windows_names,prealloc
 sleep 1
 
 if(echo $?); then
-    echo "Mounted SSDWinLower as read write, continuing"
+    echo "Mounted SSDWinLower as read write, continuing" | systemd-cat -t sysDSyncSteamb4Shutdown
 else
-    echo "Failed to mount SSDWinLower as read write, exiting"
+    echo "Failed to mount SSDWinLower as read write, exiting" | systemd-cat -t sysDSyncSteamb4Shutdown
     exit 1
 fi
-
-#check that there is enough space on the drive
 
 sudo mount UUID=78DBFD1A57D3E447 /mnt/winOverlay/SSD2WinLower -o rw,windows_names,prealloc
 sleep 1
 
 if(echo $?); then
-    echo "Mounted SSD2WinLower as read write, continuing"
+    echo "Mounted SSD2WinLower as read write, continuing" | systemd-cat -t sysDSyncSteamb4Shutdown
 else
-    echo "Failed to mount SSD2WinLower as read write, exiting"
+    echo "Failed to mount SSD2WinLower as read write, exiting" | systemd-cat -t sysDSyncSteamb4Shutdown
     exit 1
 fi
 
 
 
-echo "use overlayfs tools to merge changes"
+echo "use overlayfs tools to merge changes"  | systemd-cat -t sysDSyncSteamb4Shutdown
 sudo $SCRIPT_DIR/overlay merge -l /mnt/winOverlay/SSDWinLower/Media/Games/Steam/steamapps/ -u //mnt/winOverlay/SSDWinUpper/Media/Games/Steam/steamapps/ -f
 sudo $SCRIPT_DIR/overlay merge -l /mnt/winOverlay/SSD2WinLower/SteamLibrary/steamapps/ -u /mnt/winOverlay/SSD2WinUpper/SteamLibrary/steamapps/ -f
 
-
-
-read -n 1 -s -r -p "Press any key to continue if no errors have occurred in overlay merge"
-
-#make sure we are in root directory to avoid issues with unmounting
-
-cd /
-
-sudo umount /mnt/SSDWin
-sudo umount /mnt/SSD2Win
-
-sleep 2
-
-sudo umount /mnt/winOverlay/SSDWinLower
-sudo umount /mnt/winOverlay/SSD2WinLower
-
-sleep 2
-
-#check that the the unmounting was successful
-if mountpoint /mnt/winOverlay/SSDWinLower; then
-    echo "SSDWinLower is still writable, exiting"
-    exit 1
-fi
-
-if mountpoint /mnt/winOverlay/SSD2WinLower; then
-    echo "SSD2WinLower is still writable, exiting"
-    exit 1
-fi
-
-
-
-
-echo "remount drives from fstab"
-
-sudo mount -a && sudo systemctl daemon-reload
-sleep 1
-
-sudo mount -t overlay overlay -o noauto,x-systemd.automount,defaults,lowerdir=/mnt/winOverlay/SSDWinLower,upperdir=/mnt/winOverlay/SSDWinUpper,workdir=/mnt/winOverlay/SSDWinMerge /mnt/SSDWin
-sudo mount -t overlay overlay -o noauto,x-systemd.automount,defaults,lowerdir=/mnt/winOverlay/SSD2WinLower,upperdir=/mnt/winOverlay/SSD2WinUpper,workdir=/mnt/winOverlay/SSD2WinMerge /mnt/SSD2Win
-
+echo "Any updates to Windows Steam Library have been merged onto the NTFS partition."  | systemd-cat -t sysDSyncSteamb4Shutdown
 exit 0
 
 
---
+++ readme.txt
@@ -0,0 +1,3 @@
+This attempt uses a systemd file to run the script after user processes have been shut down, but before drives are unmounted, thus ensuring nothing will make the drives busy.
+
+server file needs to be copied to systemd directory and enabled
