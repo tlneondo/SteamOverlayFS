bc27fd9ed09d99d7aa99942ef01813613d0e130c
Travis <tlneondo@gmail.com>
2025-08-15 19:08:37 -0400

    py start?

---
+++ SteamInstallTracking.py
@@ -0,0 +1,15 @@
+class SteamLibrary:
+    path = 'new'
+
+
+def main():
+    print('Do we have a list of Steam Libraries Yet?')
+    print('we have,' path, 'now')
+
+    
+
+
+
+if __name__ == '__main__'
+    main()
+
