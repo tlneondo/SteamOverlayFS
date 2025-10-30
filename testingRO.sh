d5af18e3c9aec26354be103539f930229020f1de
tlneondo <tlneondo@gmail.com>
2025-10-30 12:20:38 -0400

    wip remounting logic

--- testingRO.sh
+++ testingRO.sh
@@ -3,62 +3,51 @@ source ./copyfunction.env
 source ./drivemounting.env
 source ./SteamACFtracking.env
 
-sudo rm ./overlay-tools*.sh
-
-killall steam
-
-sleep 5
-
-#mask systemd mounting
 sudo systemctl mask systemd-remount-fs.service
 
+
 lengthOver=${#OVERFSLOCATIONS[*]}
+
+#unmount overlays
 
 for ((i=0; i < lengthOver; i++ )); do
     unmountFinalOverlays "${OVERFSLOCATIONS[$i]}"
 done
 
-sleep 5
-
-#prompt user if un
 
 length=${#UPPERLOCATIONS[*]}
 lengthOver=${#OVERFSLOCATIONS[*]}
+lengthLower=${#LOWERLOCATIONS[*]}
+
+#remount ro ntfs drives as readable
 
 
-#generate scripts
-for ((i=0; i < length; i++ )); do
-    generateScripts ${UPPERLOCATIONS[$i]} ${LOWERLOCATIONS[$i]}
+
+for ((i=0; i < lengthLower; i++ )); do
+    echo ${i}
+    echo ${LOWERLOCATIONS[$i]}
+    remountROLowerasReadable "${LOWERLOCATIONS[$i]}"
 done
 
 
-sudo chown $USER:$USER ./overlay-tools*.sh
 
-
-#remove any linux related folders from scripts
-processScripts
-
-#run scripts
-for file in overlay-tools*.sh; do
-    sudo bash ./$file
-    #sudo bash ./$file.remove.sh
-    sync
-    sync
+#unmask lower locations
+sudo systemctl unmask   systemd-remount-fs.service
+for ((k=0; k < lengthLower; k++ )); do
+    sudo systemctl --runtime unmask "$(systemd-escape -p --suffix=automount ${LOWERLOCATIONS[$k]})"
 done
 
-#delete all in UPPERLOCATIONS
-for ((i=0; i < length; i++ )); do
-    deleteUppers ${UPPERLOCATIONS[$i]}
-done
 
-sleep 5
-
-
-#unmask
+#unmask service
 sudo systemctl unmask systemd-remount-fs.service
+
+#unmask final overlays
+sudo systemctl unmask   systemd-remount-fs.service
 for ((k=0; k < lengthOver; k++ )); do
     sudo systemctl --runtime unmask "$(systemd-escape -p --suffix=automount ${OVERFSLOCATIONS[$k]})"
 done
+
+
 
 sync
 sync
